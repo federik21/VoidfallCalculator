a0ea88af771b6c823f36e79803c8ce6707cfcbed
Federico Piccirilli <federico.piccirilli@pt.bosch.com>
2022-11-11 11:30:36 +0000

    Initial Commit

---
+++ Shared/VoidfallCombatApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoidfallCombatApp.swift
+//  Shared
+//
+//  Created by federico piccirilli on 11/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct VoidfallCombatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
