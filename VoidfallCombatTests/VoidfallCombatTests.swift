025ba6e513bbb03766b14455f69c249b0fb4732f
Federico Piccirilli <federico.piccirilli@pt.bosch.com>
2024-06-17 12:17:41 +0100

    Fix tests

--- VoidfallCombatTests/VoidfallCombatTests.swift
+++ VoidfallCombatTests/VoidfallCombatTests.swift
@@ -33,208 +33,181 @@ class VoidfallCombatTests: XCTestCase {
   }
 
   func testCombat() {
-    var technologyManager = TechnologyManager()
-    let attacker = Player(side: .invader)
-    let defender = Player(side: .defender)
-    attacker.corvettes.power = 3
-    defender.corvettes.power = 2
-    let combat = CombatViewModel(attacker: attacker, defender: defender, sectorDefenses: 0)
+    let tm = TechnologyManager()
+    let combat = CombatViewModel(technologyManager: tm)
+    combat.attacker.corvettes.power = 3
+    combat.defender.corvettes.power = 2
     combat.combat()
-    assert(attacker.power == 2)
-    assert(defender.power == 0)
+    assert(combat.attacker.power == 2)
+    assert(combat.defender.power == 0)
   }
 
   func testCombat2() {
-    var technologyManager = TechnologyManager()
-    let attacker = Player(side: .invader)
-    let defender = Player(side: .defender)
-    attacker.corvettes.power = 3
-    defender.corvettes.power = 2
-    let combat = CombatViewModel(attacker: attacker, defender: defender, sectorDefenses: 1)
+    let tm = TechnologyManager()
+    let combat = CombatViewModel(technologyManager: tm)
+    combat.attacker.corvettes.power = 3
+    combat.defender.corvettes.power = 2
+    combat.sectorDefenses = 1
     combat.combat()
-    assert(attacker.power == 0)
-    assert(defender.power == 0)
+    assert(combat.attacker.power == 0)
+    assert(combat.defender.power == 0)
   }
 
   func testCombat3() {
-    var technologyManager = TechnologyManager()
-    let attacker = Player(side: .invader)
-    let defender = Player(side: .defender)
-    attacker.corvettes.power = 3
-    defender.corvettes.power = 2
-    let combat = CombatViewModel(attacker: attacker, defender: defender, sectorDefenses: 2)
+    let tm = TechnologyManager()
+    let combat = CombatViewModel(technologyManager: tm)
+    combat.attacker.corvettes.power = 3
+    combat.defender.corvettes.power = 2
+    combat.sectorDefenses = 2
     combat.combat()
-    assert(attacker.power == 0)
-    assert(defender.power == 2)
+    assert(combat.attacker.power == 0)
+    assert(combat.defender.power == 2)
   }
 
+
   func testCombat4() {
-    var technologyManager = TechnologyManager()
-    let attacker = Player(side: .invader)
-    attacker.technologies = [.shields]
-    let defender = Player(side: .defender)
-    attacker.corvettes.power = 3
-    defender.corvettes.power = 3
-    let combat = CombatViewModel(attacker: attacker, defender: defender, sectorDefenses: 0)
+    let tm = TechnologyManager()
+    let combat = CombatViewModel(technologyManager: tm)
+    _ = tm.addTechnology(.shields, for: combat.attacker)
+    combat.attacker.corvettes.power = 3
+    combat.defender.corvettes.power = 3
     combat.combat()
-    assert(attacker.power == 2)
-    assert(defender.power == 0)
+    assert(combat.attacker.power == 2)
+    assert(combat.defender.power == 0)
   }
 
   func testCombat5() {
-    var technologyManager = TechnologyManager()
-    let attacker = Player(side: .invader)
-    attacker.technologies = [.shieldsV2]
-    let defender = Player(side: .defender)
-    attacker.corvettes.power = 3
-    defender.corvettes.power = 3
-    let combat = CombatViewModel(attacker: attacker, defender: defender, sectorDefenses: 2)
+    let tm = TechnologyManager()
+    let combat = CombatViewModel(technologyManager: tm)
+    _ = tm.addTechnology(.shieldsV2, for: combat.attacker)
+    combat.attacker.corvettes.power = 3
+    combat.defender.corvettes.power = 3
+    combat.sectorDefenses = 2
     combat.combat()
-    assert(attacker.power == 0)
-    assert(defender.power == 0)
+    assert(combat.attacker.power == 0)
+    assert(combat.defender.power == 0)
   }
 
   func testCombatCarrier() {
-    var technologyManager = TechnologyManager()
-    let attacker = Player(side: .invader)
-    let defender = Player(side: .defender)
-    attacker.corvettes.power = 1
-    attacker.carriers.power = 1
-    attacker.carriers.deployablePower = 1
-    defender.corvettes.power = 2
-    let combat = CombatViewModel(attacker: attacker, defender: defender, sectorDefenses: 0)
+    let tm = TechnologyManager()
+    let combat = CombatViewModel(technologyManager: tm)
+    combat.attacker.corvettes.power = 1
+    combat.attacker.carriers.power = 1
+    combat.attacker.carriers.deployablePower = 1
+    combat.defender.corvettes.power = 2
     combat.combat()
-    assert(attacker.power == 2)
-    assert(defender.power == 0)
+    assert(combat.attacker.power == 2)
+    assert(combat.defender.power == 0)
   }
 
   func testCombatCarrier2() {
-    var technologyManager = TechnologyManager()
-    let attacker = Player(side: .invader)
-    let defender = Player(side: .defender)
-    attacker.corvettes.power = 3
-    defender.carriers.power = 2
-    let combat = CombatViewModel(attacker: attacker, defender: defender, sectorDefenses: 0)
+    let tm = TechnologyManager()
+    let combat = CombatViewModel(technologyManager: tm)
+    combat.attacker.corvettes.power = 3
+    combat.defender.carriers.power = 2
     combat.combat()
-    assert(attacker.power == 0)
-    assert(defender.power == 2)
+    assert(combat.attacker.power == 0)
+    assert(combat.defender.power == 2)
   }
 
   func testCombatCarrier3() {
-    var technologyManager = TechnologyManager()
-    let attacker = Player(side: .invader)
-    let defender = Player(side: .defender)
-    attacker.corvettes.power = 3
-    defender.carriers.power = 1
-    let combat = CombatViewModel(attacker: attacker, defender: defender, sectorDefenses: 1)
+    let tm = TechnologyManager()
+    let combat = CombatViewModel(technologyManager: tm)
+    combat.attacker.corvettes.power = 3
+    combat.defender.carriers.power = 1
+    combat.sectorDefenses = 1
     combat.combat()
-    assert(attacker.power == 0)
-    assert(defender.power == 0)
+    assert(combat.attacker.power == 0)
+    assert(combat.defender.power == 0)
   }
 
   func testDestroyers() {
-    var technologyManager = TechnologyManager()
-    let attacker = Player(side: .invader)
-    let defender = Player(side: .defender)
-    attacker.destroyers.power = 2
-    defender.carriers.power = 1
-    defender.corvettes.power = 2
-    let combat = CombatViewModel(attacker: attacker, defender: defender, sectorDefenses: 0)
+    let tm = TechnologyManager()
+    let combat = CombatViewModel(technologyManager: tm)
+    combat.attacker.destroyers.power = 2
+    combat.defender.carriers.power = 1
+    combat.defender.corvettes.power = 2
     combat.combat()
-    assert(attacker.power == 1)
-    assert(defender.power == 0)
+    assert(combat.attacker.power == 1)
+    assert(combat.defender.power == 0)
   }
 
   func testDestroyers2() {
-    var technologyManager = TechnologyManager()
-    let attacker = Player(side: .invader)
-    let defender = Player(side: .defender)
-    attacker.destroyers.power = 2
-    defender.corvettes.power = 4
-    let combat = CombatViewModel(attacker: attacker, defender: defender, sectorDefenses: 0)
+    let tm = TechnologyManager()
+    let combat = CombatViewModel(technologyManager: tm)
+    combat.attacker.destroyers.power = 2
+    combat.defender.corvettes.power = 4
     combat.combat()
-    assert(attacker.power == 0)
-    assert(defender.power == 1)
+    assert(combat.attacker.power == 0)
+    assert(combat.defender.power == 1)
   }
 
   func testEnergyCells() {
-    var technologyManager = TechnologyManager()
-    let attacker = Player(side: .invader)
-    let defender = Player(side: .defender)
-    attacker.destroyers.power = 2
-    defender.corvettes.power = 1
-    defender.technologies.append(.energyCells)
-    let combat = CombatViewModel(attacker: attacker, defender: defender, sectorDefenses: 1)
+    let tm = TechnologyManager()
+    let combat = CombatViewModel(technologyManager: tm)
+    combat.attacker.destroyers.power = 2
+    combat.defender.corvettes.power = 1
+    _ = tm.addTechnology(.energyCells, for: combat.defender)
+    combat.sectorDefenses = 1
     combat.combat()
-    assert(attacker.power == 0)
-    assert(defender.power == 1)
+    assert(combat.attacker.power == 0)
+    assert(combat.defender.power == 1)
   }
 
   func testSentry() {
-    var technologyManager = TechnologyManager()
-    let attacker = Player(side: .invader)
-    let defender = Player(side: .defender)
-    attacker.corvettes.power = 3
-    defender.sentries.power = 2
-    let combat = CombatViewModel(attacker: attacker, defender: defender, sectorDefenses: 0)
+    let tm = TechnologyManager()
+    let combat = CombatViewModel(technologyManager: tm)
+    combat.attacker.corvettes.power = 3
+    combat.defender.sentries.power = 2
     combat.combat()
-    assert(attacker.power == 1)
-    assert(defender.power == 0)
+    assert(combat.attacker.power == 1)
+    assert(combat.defender.power == 0)
   }
 
   func testTargeting() {
-    var technologyManager = TechnologyManager()
-    let defender = Player(side: .defender)
-    defender.corvettes.power = 1
-    defender.destroyers.power = 1
-    defender.sentries.power = 2
-    defender.technologies.append(.targeting)
-    assert(defender.initiative == 7)
-
-    defender.corvettes.power = 0
-    defender.destroyers.power = 0
-//     Still has sentries
-    assert(defender.initiative == 0)
+    let tm = TechnologyManager()
+    let combat = CombatViewModel(technologyManager: tm)
+    combat.defender.corvettes.power = 2
+    _ = tm.addTechnology(.targeting, for: combat.defender)
+    assert(combat.defender.initiative == 7)
+    combat.defender.corvettes.power = 0
+    assert(combat.defender.initiative == 0)
   }
+
   func testTorpedoes() {
-    var technologyManager = TechnologyManager()
-    let attacker = Player(side: .invader)
-    let defender = Player(side: .defender)
-    attacker.corvettes.power = 2
-    attacker.technologies = [.torpedoes]
-    defender.corvettes.power = 2
-    let combat = CombatViewModel(attacker: attacker, defender: defender, sectorDefenses: 0)
+    let tm = TechnologyManager()
+    let combat = CombatViewModel(technologyManager: tm)
+    combat.attacker.corvettes.power = 2
+    _ = tm.addTechnology(.torpedoes, for: combat.attacker)
+    combat.defender.corvettes.power = 2
     combat.combat()
-    assert(attacker.power == 1)
-    assert(defender.power == 0)
+    assert(combat.attacker.power == 1)
+    assert(combat.defender.power == 0)
   }
 
   func testTorpedoesV2() {
-    var technologyManager = TechnologyManager()
-    let attacker = Player(side: .invader)
-    let defender = Player(side: .defender)
-    attacker.corvettes.power = 4
-    attacker.technologies = [.torpedoesV2]
-    defender.corvettes.power = 6
-    let combat = CombatViewModel(attacker: attacker, defender: defender, sectorDefenses: 0)
+    let tm = TechnologyManager()
+    let combat = CombatViewModel(technologyManager: tm)
+    combat.attacker.corvettes.power = 4
+    _ = tm.addTechnology(.torpedoesV2, for: combat.attacker)
+    combat.defender.corvettes.power = 6
     combat.combat()
-    assert(attacker.power == 1)
-    assert(defender.power == 0)
+    assert(combat.attacker.power == 1)
+    assert(combat.defender.power == 0)
   }
 
   func testACH() {
-    var technologyManager = TechnologyManager()
-    let attacker = Player(side: .invader)
-    let defender = Player(side: .defender)
-    attacker.corvettes.power = 1
-    attacker.dreadnoughts.power = 3
-    attacker.technologies = [.torpedoesV2]
-    defender.dreadnoughts.power = 1
-    defender.corvettes.power = 2
-    attacker.technologies = [.shields]
-
-    let combat = CombatViewModel(attacker: attacker, defender: defender, sectorDefenses: 0)
+    let tm = TechnologyManager()
+    let combat = CombatViewModel(technologyManager: tm)
+    combat.attacker.corvettes.power = 1
+    combat.attacker.dreadnoughts.power = 3
+    _ = tm.addTechnology(.torpedoesV2, for: combat.attacker)
+    _ = tm.addTechnology(.shields, for: combat.attacker)
+    combat.defender.dreadnoughts.power = 1
+    combat.defender.corvettes.power = 2
     combat.combat()
 
+    assert(combat.attacker.power == 4)
+    assert(combat.defender.power == 0)
   }
 }
